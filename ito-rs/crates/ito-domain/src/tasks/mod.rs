5aae93200c9573c85a2162b4b3578721aed18d6f
withakay <jack@fader.co.uk>
2026-02-11 02:18:42 +0000

    security(ito-domain): harden tasks path construction

--- ito-rs/crates/ito-domain/src/tasks/mod.rs
+++ ito-rs/crates/ito-domain/src/tasks/mod.rs
@@ -10,6 +10,7 @@ pub use parse::detect_tasks_format;
 pub use parse::enhanced_tasks_template;
 pub use parse::parse_tasks_tracking_file;
 pub use parse::tasks_path;
+pub use parse::tasks_path_checked;
 pub use repository::TaskRepository;
 pub use update::update_checkbox_task_status;
 pub use update::update_enhanced_task_status;
